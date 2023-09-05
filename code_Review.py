369796c3d29874ed069cb0fd37e08bc6a9339a7b
Ipsa Yadav <56242371+ixy40@users.noreply.github.com>
2023-09-05 14:36:30 -0400

    Update code_Review.py

--- code_Review.py
+++ code_Review.py
@@ -1,10 +1,35 @@
-improt math
+import numpy as np
+import matplotlib.pyplot as plt
 
-# Input angle in radians
-angle_in_radians = float(input("Enter the angle in radians: "))
+# Create an array of x values from -2*pi to 2*pi
+x = np.linspace(-2 * np.pi, 2 * np.pi, 1000)
 
-# Calculate the sine of the angle
-sine_value = math.sin(angle_in_radians)
+# Calculate the sine values for each x
+y = np.sin(x)
 
-# Print the result
-print(f"The sign of {angle_in_radians} radians is {sine_value}")
+# Create a plot 
+plt.figure(figsize=(8, 4))
+plt.plot(x, y, label='sin(x)', color='blue')
+
+# Add labels and a legend 
+    plt.xlabel('x')  
+   plt.ylabel('sin(x)')  
+  plt.title('Sine Function')  
+ plt.legend() 
+
+# Show the plot 
+plt.grid(True)
+plt.show()
+
+x = [-2 * np.pi, -np.pi, 0, np.pi, 2 * np.pi]
+y = np.sin(x)  
+
+# Plot the sine function 
+plt.figure(figsize=(8, 4))
+plt.plot(x, y, label='sin(x)', color='red')
+plt.xlabel('x')
+plt.ylabel('sin(x)')
+plt.title('Sine Function (Incorrect Data Structure)')
+plt.legend()
+plt.grid(True)
+plt.show()
