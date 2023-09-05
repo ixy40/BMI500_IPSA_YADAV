fed67857f9e3be40f8dfb8181d086f3b9b0bb965
Ipsa Yadav <56242371+ixy40@users.noreply.github.com>
2023-09-05 11:33:09 -0400

    Add files via upload

---
+++ code_Review.py
@@ -0,0 +1,10 @@
+improt math
+
+# Input angle in radians
+angle_in_radians = float(input("Enter the angle in radians: "))
+
+# Calculate the sine of the angle
+sine_value = math.sin(angle_in_radians)
+
+# Print the result
+print(f"The sign of {angle_in_radians} radians is {sine_value}")
